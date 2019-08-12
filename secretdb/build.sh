4346775f3aca2516993ab9cd35fa3d5595e5c1e4
Karl Wallenius <karl.wallenius@redeploy.se>
2019-08-12 22:21:06 +0200

    Updated build script

--- secretdb/build.sh
+++ secretdb/build.sh
@@ -7,10 +7,11 @@ fi
 
 VERSION=$1
 OS=linux
+BIN=secretdb
 # Test and build with make.
 make VERSION=$VERSION release -j3 || { echo 'FAILURE: test/build failed'; exit 1;}
 # Rename and copy file
 mkdir -p release/bin
-cp release/secretdb-$VERSION-$OS-amd64 release/bin/secretdb
+cp release/$BIN-$VERSION-$OS-amd64 release/bin/$BIN
 # Create docker image.
-docker build -t secretdb:$VERSION --build-arg VERSION=$VERSION .
+docker build -t $BIN:$VERSION --build-arg VERSION=$VERSION .
