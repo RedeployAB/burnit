419a9ba44e8c2fbe8c5f4893ab7cf26ac5a5c2cb
Karl Wallenius <karl.wallenius@redeploy.se>
2019-08-14 16:23:17 +0200

    Added middleware and logger

--- secretgen/main.go
+++ secretgen/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/RedeployAB/redeploy-secrets/secretgen/api"
 	"github.com/RedeployAB/redeploy-secrets/secretgen/config"
+	"github.com/RedeployAB/redeploy-secrets/secretgen/middleware"
 	"github.com/gorilla/mux"
 )
 
@@ -17,8 +18,8 @@ func main() {
 	// Routes.
 	r.HandleFunc("/api/"+apiVer+"/secret", api.GenerateSecretHandler).Methods("GET")
 	r.PathPrefix("/").HandlerFunc(api.NotFoundHandler)
+	r.Use(middleware.Logger)
 	// Start server.
 	log.Printf("server listening on: %s", port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
-
 }
