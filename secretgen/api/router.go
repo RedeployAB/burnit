f01394a434121f929c3f8cfcda37ef0001e5863b
Karl Wallenius <karl.wallenius@redeploy.se>
2019-08-15 18:57:25 +0200

    Fixed handler name

--- secretgen/api/router.go
+++ secretgen/api/router.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"github.com/RedeployAB/redeploy-secrets/common/middleware"
-	"github.com/RedeployAB/redeploy-secrets/secretapi/api"
 	"github.com/RedeployAB/redeploy-secrets/secretgen/config"
 	"github.com/gorilla/mux"
 )
@@ -14,7 +13,7 @@ func NewRouter(config config.Server) *mux.Router {
 	r := mux.NewRouter()
 	// Routes.
 	r.HandleFunc("/api/"+apiVer+"/generate", generateSecretHandler).Methods("GET")
-	r.PathPrefix("/").HandlerFunc(api.NotFoundHandler)
+	r.PathPrefix("/").HandlerFunc(notFoundHandler)
 	r.Use(middleware.Logger)
 
 	return r
