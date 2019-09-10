eda6ac4bc7397c6a7ab651faab3e85121514c159
Karl Wallenius <karl.wallenius@redeploy.se>
2019-09-10 21:43:47 +0200

    Updated env variable name to 'SECRET_GW_SERVICE_PORT'

--- secretgw/config/config.go
+++ secretgw/config/config.go
@@ -25,7 +25,7 @@ type Configuration struct {
 // Configure performs the necessary steps
 // for server/app configuration.
 func Configure() Configuration {
-	port := os.Getenv("SECRET_API_SERVICE_PORT")
+	port := os.Getenv("SECRET_GW_SERVICE_PORT")
 	if port == "" {
 		port = "3000"
 	}
