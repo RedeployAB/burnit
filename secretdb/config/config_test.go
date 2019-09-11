fbee9c26806e830647ce708ac5a06c1651788589
Karl Wallenius <karl.wallenius@redeploy.se>
2019-09-11 21:15:25 +0200

    Updated test to use correct environment variable

--- secretdb/config/config_test.go
+++ secretdb/config/config_test.go
@@ -11,9 +11,8 @@ func TestConfigure(t *testing.T) {
 		t.Errorf("Default port value is incorrect, got %s, want: 3001", confDefault.Port)
 	}
 
-	os.Setenv("SECRET_DB_PORT", "6000")
+	os.Setenv("SECRET_DB_SERVICE_PORT", "6000")
 	os.Setenv("SECRET_GENERATOR_BASE_URL", "http://generator:7000")
-	os.Setenv("SECRET_GENERATOR_REQUEST_PATH", "/api/v2/secret")
 	confEnv := Configure()
 	if confEnv.Port != "6000" {
 		t.Errorf("Port value is incorrect, got %s, want: 6000", confEnv.Port)
