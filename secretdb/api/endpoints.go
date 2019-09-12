a7010c8bbb50fc2b11c0f81df8ebca00520be48b
Karl Wallenius <karl.wallenius@redeploy.se>
2019-09-13 01:16:23 +0200

    Implemented new driver, fixed create endpoint, need to fix read and delete

--- secretdb/api/endpoints.go
+++ secretdb/api/endpoints.go
@@ -9,7 +9,8 @@ import (
 	"github.com/RedeployAB/redeploy-secrets/secretdb/db"
 	"github.com/RedeployAB/redeploy-secrets/secretdb/internal"
 	"github.com/gorilla/mux"
-	"gopkg.in/mgo.v2"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // NotFoundHandler handles all non used routes.
@@ -19,39 +20,40 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // ReadSecretHandler reads a secret fron the database by ID.
-func readSecretHandler(collection *mgo.Collection) http.Handler {
+func readSecretHandler(client *mongo.Client) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		vars := mux.Vars(r)
 
-		sm, err := db.Find(vars["id"], collection)
+		res, err := db.Find(vars["id"], collection)
 		if err != nil {
 			httperror.Error(w, "not found", http.StatusNotFound)
 			return
 		}
 		// Handle if passphrase is set on the secret.
-		if len(sm.Passphrase) > 0 && !internal.CompareHash(sm.Passphrase, r.Header.Get("X-Passphrase")) {
+		if len(res.Passphrase) > 0 && !internal.CompareHash(res.Passphrase, r.Header.Get("X-Passphrase")) {
 			httperror.Error(w, "unauthorized", http.StatusUnauthorized)
 			return
 		}
 
-		db.Delete(vars["id"], collection)
-		sr := secretResponseBody{
-			Data: secretData{
-				Secret:    sm.Secret,
-				CreatedAt: sm.CreatedAt,
-				ExpiresAt: sm.ExpiresAt,
+		db.Delete(vars["id"], client)
+		rb := ResponseBody{
+			Data: Data{
+				Secret:    res.Secret,
+				CreatedAt: res.CreatedAt,
+				ExpiresAt: res.ExpiresAt,
 			},
 		}
 		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(&sr); err != nil {
+		if err := json.NewEncoder(w).Encode(&rb); err != nil {
 			panic(err)
 		}
+
 	})
 }
 
 // CreateSecretHandler inserts a secret into the database.
-func createSecretHandler(collection *mgo.Collection) http.Handler {
+func createSecretHandler(client *mongo.Client) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		var s db.Secret
@@ -60,24 +62,23 @@ func createSecretHandler(collection *mgo.Collection) http.Handler {
 			return
 		}
 
-		sm, err := db.Insert(s, collection)
+		res, err := db.Insert(s, client)
 		if err != nil {
 			httperror.Error(w, "internal server error", http.StatusInternalServerError)
-			return
 		}
 
 		w.WriteHeader(http.StatusCreated)
-		sr := secretResponseBody{
-			Data: secretData{ID: sm.ID.Hex(), CreatedAt: sm.CreatedAt, ExpiresAt: sm.ExpiresAt},
+		rb := &ResponseBody{
+			Data: Data{ID: res.ID, CreatedAt: res.CreatedAt, ExpiresAt: res.ExpiresAt},
 		}
-		if err := json.NewEncoder(w).Encode(&sr); err != nil {
+		if err := json.NewEncoder(w).Encode(rb); err != nil {
 			panic(err)
 		}
 	})
 }
 
 // UpdateSecretHandler handler updates a secret in the database.
-func updateSecretHandler(collection *mgo.Collection) http.Handler {
+func updateSecretHandler(client *mongo.Client) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		httperror.Error(w, "not implemented", http.StatusNotImplemented)
@@ -85,34 +86,48 @@ func updateSecretHandler(collection *mgo.Collection) http.Handler {
 }
 
 // DeleteSecretHandler deletes a secret from the database.
-func deleteSecretHandler(collection *mgo.Collection) http.Handler {
+func deleteSecretHandler(client *mongo.Client) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-		vars := mux.Vars(r)
+		/* 		vars := mux.Vars(r)
 
-		err := db.Delete(vars["id"], collection)
-		if err != nil {
-			if err.Error() == "not found" || err.Error() == "not valid ObjectId" {
-				httperror.Error(w, "not found", http.StatusNotFound)
-			} else {
-				httperror.Error(w, "internal server error", http.StatusInternalServerError)
-			}
-			return
-		}
+		   		err := db.Delete(vars["id"], collection)
+		   		if err != nil {
+		   			if err.Error() == "not found" || err.Error() == "not valid ObjectId" {
+		   				httperror.Error(w, "not found", http.StatusNotFound)
+		   			} else {
+		   				httperror.Error(w, "internal server error", http.StatusInternalServerError)
+		   			}
+		   			return
+		   		} */
 
 		w.WriteHeader(http.StatusOK)
 	})
 }
 
-// SecretResponseBody represents a secret type response.
-type secretResponseBody struct {
-	Data secretData `json:"data"`
+// ResponseBody represents a secret type response body.
+type ResponseBody struct {
+	Data Data `json:"data"`
 }
 
-type secretData struct {
+// Data represents the data part of the response body.
+type Data struct {
 	ID        string    `json:"id,omitempty"`
 	Secret    string    `json:"secret,omitempty"`
-	CreatedAt time.Time `json:"created_at"`
-	ExpiresAt time.Time `json:"expires_at"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	ExpiresAt time.Time `json:"expires_at,omitempty"`
 }
+
+// RequestBody represents a secret request body.
+type RequestBody struct {
+	Secret     string `json:"secret"`
+	Passphrase string `json:"passphrase,omitempty"`
+}
+
+/* type Secret struct {
+	Secret     string    `bson:"secret"`
+	Passphrase string    `bson:"passphrase,omitempty"`
+	CreatedAt  time.Time `bson:"created_at"`
+	ExpiresAt  time.Time `bson:"expires_at"`
+} */
