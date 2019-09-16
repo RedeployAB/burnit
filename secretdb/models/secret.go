e0e0b128dd0a731f214d85cb4faf345e63840373
Karl Wallenius <karl.wallenius@redeploy.se>
2019-09-17 01:09:00 +0200

    Updated model to handle BSON and ObjectID correctly, and JSON transform updates

--- secretdb/models/secret.go
+++ secretdb/models/secret.go
@@ -1,11 +1,16 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 // Secret represents a secret entry in database.
 type Secret struct {
-	Secret     string    `bson:"secret"`
-	Passphrase string    `bson:"passphrase,omitempty"`
-	CreatedAt  time.Time `bson:"created_at"`
-	ExpiresAt  time.Time `bson:"expires_at"`
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Secret     string             `bson:"secret" json:"secret,omitempty"`
+	Passphrase string             `bson:"passphrase,omitempty" json:"passphrase,omitempty"`
+	CreatedAt  time.Time          `bson:"created_at" json:"created_at,omitempty"`
+	ExpiresAt  time.Time          `bson:"expires_at" json:"expires_at,omitempty"`
 }
