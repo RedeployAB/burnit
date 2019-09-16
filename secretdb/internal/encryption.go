9530103d3c4c3997f2254538624c2dd0b7672f4a
Karl Wallenius <karl.wallenius@redeploy.se>
2019-09-17 01:14:03 +0200

    Updated encryption functions with modifications to: https://github.com/gtank/cryptopasta

--- secretdb/internal/encryption.go
+++ secretdb/internal/encryption.go
@@ -5,48 +5,86 @@ import (
 	"crypto/cipher"
 	"crypto/md5"
 	"crypto/rand"
+	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"io"
 )
 
-func hash(key string) string {
+// Source: https://github.com/gtank/cryptopasta
+// Modified to encode/decode from base64 and a passphrase string
+// and encode it to MD5. Some minor modifictans in byte/string
+// handling ass well. Encrypts outout a base64 encoded string,
+// Decrypt takes a base64 encoded string.
+
+// Encrypt encrypts data using 256-bit AES-GCM.  This both hides the content of
+// the data and provides a check that it hasn't been altered. Output takes the
+// form nonce|ciphertext|tag where '|' indicates concatenation.
+func Encrypt(plaintext []byte, key string) (ciphertext []byte, err error) {
+	md5key := ToMD5(key)
+	block, err := aes.NewCipher([]byte(md5key))
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(encodeBase64(gcm.Seal(nonce, nonce, plaintext, nil))), nil
+}
+
+// Decrypt decrypts data using 256-bit AES-GCM.  This both hides the content of
+// the data and provides a check that it hasn't been altered. Expects input
+// form nonce|ciphertext|tag where '|' indicates concatenation.
+func Decrypt(ciphertext []byte, key string) (plaintext []byte, err error) {
+	md5Key := ToMD5(key)
+	block, err := aes.NewCipher([]byte(md5Key))
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	decodedCipher := decodeBase64(ciphertext)
+	if len(decodedCipher) < gcm.NonceSize() {
+		return nil, errors.New("malformed ciphertext")
+	}
+
+	return gcm.Open(nil,
+		decodedCipher[:gcm.NonceSize()],
+		decodedCipher[gcm.NonceSize():],
+		nil,
+	)
+}
+
+// ToMD5 hashes a string to MD5.
+func ToMD5(key string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(key))
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-// Encrypt encrypts provided data with AES.
-func Encrypt(data []byte, passphrase string) []byte {
-	block, _ := aes.NewCipher([]byte(hash(passphrase)))
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err)
-	}
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err)
-	}
-
-	return gcm.Seal(nonce, nonce, data, nil)
+// Encodes a byte containing a string to base64.
+func encodeBase64(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
 }
 
-// Decrypt decrypts provided data.
-func Decrypt(data []byte, passphrase string) []byte {
-	key := []byte(hash(passphrase))
-	block, err := aes.NewCipher(key)
+// Decodes a byte slice containing a base64 value to a byte slice.
+func decodeBase64(b []byte) []byte {
+	data, err := base64.StdEncoding.DecodeString(string(b))
 	if err != nil {
 		panic(err)
 	}
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err)
-	}
-	nonceSize := gcm.NonceSize()
-	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	return plaintext
+	return data
 }
