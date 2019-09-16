b0a118e8a42b097a238f68750b3827de450a510b
Karl Wallenius <karl.wallenius@redeploy.se>
2019-09-17 01:15:36 +0200

    Updated how Insert is done, added Find and Delete implementations

--- secretdb/db/actions.go
+++ secretdb/db/actions.go
@@ -4,48 +4,86 @@ import (
 	"context"
 	"time"
 
+	"github.com/RedeployAB/redeploy-secrets/secretdb/config"
+	"github.com/RedeployAB/redeploy-secrets/secretdb/internal"
 	"github.com/RedeployAB/redeploy-secrets/secretdb/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Find queries the collection for an entry by ID.
-func Find(id string, client *mongo.Client) (Secret, error) {
-
-}
-
-// Insert handles inserts into the database.
-func Insert(s Secret, client *mongo.Client) (Secret, error) {
-
-	sm := &models.Secret{
-		Secret:    s.Secret,
-		CreatedAt: time.Now(),
-		ExpiresAt: time.Now().AddDate(0, 0, 7),
+func Find(id string, client *mongo.Client) (models.Secret, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return models.Secret{}, err
 	}
 
 	collection := client.Database("secretdb").Collection("secrets")
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 
+	var s models.Secret
+	err = collection.FindOne(ctx, bson.D{{"_id", oid}}).Decode(&s)
+	if err != nil {
+		return models.Secret{}, err
+	}
+	s.Secret = decrypt(s.Secret, config.Config.Passphrase)
+
+	return s, nil
+}
+
+// Insert handles inserts into the database.
+func Insert(s models.Secret, client *mongo.Client) (models.Secret, error) {
+	collection := client.Database("secretdb").Collection("secrets")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+	sm := &models.Secret{
+		Secret:    encrypt(s.Secret, config.Config.Passphrase),
+		CreatedAt: time.Now(),
+		ExpiresAt: time.Now().AddDate(0, 0, 7),
+	}
+
 	res, err := collection.InsertOne(ctx, sm)
 	if err != nil {
-		return Secret{}, err
+		return models.Secret{}, err
 	}
-	oid := res.InsertedID.(primitive.ObjectID).Hex()
+	oid := res.InsertedID.(primitive.ObjectID)
 
-	return Secret{ID: oid, CreatedAt: sm.CreatedAt, ExpiresAt: sm.ExpiresAt}, nil
+	return models.Secret{ID: oid, CreatedAt: sm.CreatedAt, ExpiresAt: sm.ExpiresAt}, nil
 }
 
 // Delete removes an entry from the collection by ID.
-func Delete(id string, client *mongo.Client) error {
+func Delete(id string, client *mongo.Client) (int64, error) {
+	collection := client.Database("secretdb").Collection("secrets")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return 0, err
+	}
+
+	res, err := collection.DeleteOne(ctx, bson.D{{"_id", oid}})
+	if err != nil {
+		return 0, err
+	}
+
+	return res.DeletedCount, nil
 }
 
-// Secret represents a secret to be inserted into the
-// database collection.
-type Secret struct {
-	ID         string    `json:"id,omitempty"`
-	Secret     string    `json:"secret,omitempty"`
-	Passphrase string    `json:"passphrase,omitempty"`
-	CreatedAt  time.Time `json:"created_at,omitempty"`
-	ExpiresAt  time.Time `json:"expires_at,omitempty"`
+// Wrapper around internal.Encrypt to ease usage.
+func encrypt(plaintext, passphrase string) string {
+	encrypted, err := internal.Encrypt([]byte(plaintext), passphrase)
+	if err != nil {
+		panic(err)
+	}
+	return string(encrypted)
+}
+
+// Wrapper around internal.Decrupt to ease usage.
+func decrypt(ciphertext, passphrase string) string {
+	decrypted, err := internal.Decrypt([]byte(ciphertext), passphrase)
+	if err != nil {
+		panic(err)
+	}
+	return string(decrypted)
 }
