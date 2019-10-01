519d5aefddd4607a7395b5e7d95000170825e122
Karl Wallenius <karl.wallenius@redeploy.se>
2019-10-01 23:06:48 +0200

    Renamed internal names, and renamed file to responses

--- secretdb/api/response.go
+++
@@ -1,6 +0,0 @@
-package api
-
-// secretResponse represents a secret type response body.
-type secretResponse struct {
-	Data secret `json:"data"`
-}
---
+++ secretdb/api/responses.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"time"
+)
+
+// secretResponse Bodyrepresents a secret type response body.
+type secretResponseBody struct {
+	Data secretResponse `json:"data"`
+}
+
+// secretResponse represents the data part of the response body.
+type secretResponse struct {
+	ID        string    `json:"id,omitempty"`
+	Secret    string    `json:"secret,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+	ExpiresAt time.Time `json:"expiresAt,omitempty"`
+}
