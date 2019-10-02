70b479a05581d76d6824bffdef47423e70dac378
Karl Wallenius <karl.wallenius@redeploy.se>
2019-10-02 20:40:52 +0200

    Added property ttl, and new creator func to create the secret obj

--- secretdb/secret/secret.go
+++ secretdb/secret/secret.go
@@ -1,6 +1,8 @@
 package secret
 
 import (
+	"encoding/json"
+	"io"
 	"time"
 
 	"github.com/RedeployAB/redeploy-secrets/common/security"
@@ -14,6 +16,24 @@ type Secret struct {
 	Passphrase string
 	CreatedAt  time.Time
 	ExpiresAt  time.Time
+	TTL        int
+}
+
+// NewSecret takes an io.Reader and attempt to decode it to
+// a Secret.
+func NewSecret(b io.ReadCloser) (*Secret, error) {
+	var s Secret
+	if err := json.NewDecoder(b).Decode(&s); err != nil {
+		return &Secret{}, err
+	}
+
+	exp := time.Now().AddDate(0, 0, 7)
+	if s.TTL != 0 {
+		exp = time.Now().Add(time.Minute * time.Duration(s.TTL))
+	}
+	s.ExpiresAt = exp
+
+	return &s, nil
 }
 
 // VerifyPassphrase compares a hash with a string,
