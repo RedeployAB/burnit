15ed48669f2939fabede5ea087346d5c7e2ad623
Karl Wallenius <karl.wallenius@redeploy.se>
2019-10-06 23:29:59 +0200

    Removed DB-service related errors

--- secretgw/internal/errors.go
+++ secretgw/internal/errors.go
@@ -1,21 +1,9 @@
 package internal
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
-
-// DBRequestError implements error interface and represents
-// errors to secretdb
-type DBRequestError struct {
-	err  string
-	code int
-}
-
-func (e *DBRequestError) Error() string {
-	return fmt.Sprintf("code %d: %s", e.code, e.err)
-}
 
 // HandleHTTPError takes an error and parses it for HTTP errors.
 func HandleHTTPError(err error) int {
