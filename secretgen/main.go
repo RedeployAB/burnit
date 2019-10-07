73f87ec8ed63968eed117d9f9ad8da711110b29c
Karl Wallenius <karl.wallenius@redeploy.se>
2019-10-07 23:48:34 +0200

    Updated server handling, struct and methods

--- secretgen/main.go
+++ secretgen/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-
 	"github.com/RedeployAB/redeploy-secrets/secretgen/api"
 	"github.com/RedeployAB/redeploy-secrets/secretgen/config"
 	"github.com/RedeployAB/redeploy-secrets/secretgen/server"
@@ -15,11 +13,6 @@ func main() {
 	conf := config.Configure()
 	r := api.NewRouter()
 	srv := server.NewServer(conf, r)
-
-	done := make(chan bool, 1)
-	go srv.AddShutdownHook(done)
 	// Start server.
 	srv.Start()
-	<-done
-	log.Println("server has been stopped")
 }
--- secretgen/server/server.go
+++ secretgen/server/server.go
@@ -20,34 +20,36 @@ type Server struct {
 
 // NewServer returns a configured Server.
 func NewServer(config config.Configuration, r *mux.Router) *Server {
-	srv := &Server{
-		&http.Server{
-			Addr:         "0.0.0.0:" + config.Port,
-			WriteTimeout: time.Second * 15,
-			ReadTimeout:  time.Second * 15,
-			IdleTimeout:  time.Second * 60,
-			Handler:      r,
-		}, config.Port,
+	srv := &http.Server{
+		Addr:         "0.0.0.0:" + config.Port,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r,
 	}
 
-	return srv
+	return &Server{srv, config.Port}
 }
 
 // Start creates an http server and runs ListenAndServe().
 func (s *Server) Start() {
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server err: %v\n", err)
+		}
+	}()
 	log.Printf("server listening on: %s", s.Port)
-	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("server err: %v\n", err)
-	}
+	s.gracefulShutdown()
 }
 
-// AddShutdownHook awaits os.Interrupt and os.Kill to start
-// shutdown on server.
-func (s *Server) AddShutdownHook(done chan<- bool) {
+// gracefulShutdown will shutdown server when interrupt or
+// kill is received.
+func (s *Server) gracefulShutdown() {
 	stop := make(chan os.Signal, 1)
+
 	signal.Notify(stop, os.Interrupt, os.Kill)
-	<-stop
-	log.Println("shutting down server...")
+	sig := <-stop
+	log.Printf("shutting down server. reason: %s\n", sig.String())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
@@ -56,5 +58,5 @@ func (s *Server) AddShutdownHook(done chan<- bool) {
 	if err := s.Shutdown(ctx); err != nil {
 		log.Fatalf("could not shutdown server gracefully: %v", err)
 	}
-	close(done)
+	log.Println("server has been stopped")
 }
