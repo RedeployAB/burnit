f630da9acc6d6468879f89f31ca15eb2e44c9d4b
Karl Wallenius <karl.wallenius@redeploy.com>
2020-01-11 15:35:37 +0100

    Added tests for server

---
+++ secretgw/app/server_test.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/RedeployAB/burnit/secretgw/config"
+	"github.com/gorilla/mux"
+)
+
+func TestNewServer(t *testing.T) {
+
+	conf := config.Configuration{config.Server{Port: "5000"}}
+	r := mux.NewRouter()
+	srv := NewServer(conf, r)
+
+	if srv.httpServer.Addr != "0.0.0.0:5000" {
+		t.Errorf("Incorrect value, got: %s, want: 0.0.0.0:5000", srv.httpServer.Addr)
+	}
+}
+
+func TestStartAndShutdown(t *testing.T) {
+	conf := config.Configuration{config.Server{Port: "5000"}}
+	r := mux.NewRouter()
+	srv := NewServer(conf, r)
+
+	proc, err := os.FindProcess(os.Getpid())
+	if err != nil {
+		t.Fatal("error in getting running process")
+	}
+
+	var result *os.ProcessState
+
+	go func() {
+		srv.Start()
+		result, _ = proc.Wait()
+	}()
+
+	time.Sleep(3 * time.Second)
+	proc.Signal(os.Interrupt)
+
+	exitCode := result.ExitCode()
+	if exitCode != -1 {
+		t.Errorf("Incorrect value, got: %d, want: -1", exitCode)
+	}
+}
