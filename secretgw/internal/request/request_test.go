95feff1dbbf1e4501179e2b7be7295c22db85673
Karl Wallenius <karl.wallenius@redeploy.com>
2020-01-12 14:41:40 +0100

    Added tests for package request

---
+++ secretgw/internal/request/request_test.go
@@ -0,0 +1,159 @@
+package request
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type mockResponse struct {
+	Secret string `json:"secret"`
+}
+
+type mockFullResponse struct {
+	Data mockResponse
+}
+
+func TestBasicRequest(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mr := ResponseBody{
+			Data: mockResponse{
+				Secret: "secret",
+			},
+		}
+		json.NewEncoder(w).Encode(&mr)
+	}))
+	defer srv.Close()
+
+	client := NewHTTPClient(srv.URL, "/path")
+	opts := Options{Method: GET}
+	res, err := client.Request(opts)
+	if err != nil {
+		t.Fatalf("Error in setting client call: %v", err)
+	}
+
+	// Update this to use type assertion later on.
+	jsonRes, err := json.Marshal(res)
+	if err != nil {
+		t.Fatalf("Error in JSON marshaling: %v", err)
+	}
+
+	if string(jsonRes) != `{"data":{"secret":"secret"}}` {
+		t.Errorf(`Incorrect value, got: %s, want: {"data":{"secret":"secret"}}`, string(jsonRes))
+	}
+}
+
+func TestBasicRequestError(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(400)
+		return
+	}))
+	defer srv.Close()
+
+	client := NewHTTPClient(srv.URL, "/path")
+	opts := Options{Method: GET}
+	_, err := client.Request(opts)
+	if err == nil {
+		t.Fatal("Incorrect, should result in an error")
+	}
+
+	if err.(*Error).code != 400 {
+		t.Errorf("Incorrect value, got: %v, want: 400", err.(*Error).code)
+	}
+	if err.(*Error).Error() != "code 400: 400 Bad Request" {
+		t.Errorf("Incorrect value, got: %s, want: code 400: Bad Request", err.(*Error).Error())
+	}
+}
+
+func TestPostRequestWithoutBody(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mr := ResponseBody{}
+		json.NewEncoder(w).Encode(&mr)
+	}))
+
+	client := NewHTTPClient(srv.URL, "/path")
+	opts := Options{Method: POST}
+	_, err := client.Request(opts)
+
+	if err == nil {
+		t.Fatal("Incorrect, should result in an error")
+	}
+
+	if err.(*Error).code != 400 {
+		t.Errorf("Incorrect value, got: %v, want: 400", err.(*Error).code)
+	}
+	if err.(*Error).Error() != "code 400: 400 Bad Request" {
+		t.Errorf("Incorrect value, got: %s, want: code 400: Bad Request", err.(*Error).Error())
+	}
+}
+
+func TestRequestWithParams(t *testing.T) {
+	expectedParam := "1234"
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.URL.String(), "/1234") {
+			w.WriteHeader(400)
+			return
+		}
+		mr := ResponseBody{
+			Data: mockResponse{
+				Secret: "secret",
+			},
+		}
+		json.NewEncoder(w).Encode(&mr)
+	}))
+	defer srv.Close()
+
+	client := NewHTTPClient(srv.URL, "/path")
+	params := map[string]string{"id": expectedParam}
+	opts := Options{Method: GET, Params: params}
+	_, err := client.Request(opts)
+	if err != nil {
+		t.Errorf("Incorrect value, got: %v, want: <nil>", err)
+	}
+}
+
+func TestBasicRequestWithQuery(t *testing.T) {
+	expectedQuery := "length=10"
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.URL.String(), "?"+expectedQuery) {
+			w.WriteHeader(400)
+			return
+		}
+		mr := ResponseBody{
+			Data: mockResponse{
+				Secret: "secret",
+			},
+		}
+		json.NewEncoder(w).Encode(&mr)
+	}))
+	defer srv.Close()
+
+	client := NewHTTPClient(srv.URL, "/path")
+	opts := Options{Method: GET, Query: expectedQuery}
+	_, err := client.Request(opts)
+	if err != nil {
+		t.Errorf("Incorrect value, got: %v, want: <nil>", err)
+	}
+}
+
+func TestBasicRequestWithHeaders(t *testing.T) {
+	hdrName := "X-PASSPHRASE"
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hdrVal := r.Header.Get(hdrName)
+		if len(hdrVal) == 0 {
+			t.Error("Incorrect, header should be set.")
+			return
+		}
+		mr := ResponseBody{}
+		json.NewEncoder(w).Encode(&mr)
+	}))
+
+	client := NewHTTPClient(srv.URL, "/path")
+
+	hdr := http.Header{}
+	hdr.Add(hdrName, "test")
+	opts := Options{Method: GET, Header: hdr}
+	_, _ = client.Request(opts)
+}
