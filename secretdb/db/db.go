855d96bf700f020030581c80eeb64608ec9510c0
Karl Wallenius <karl.wallenius@redeploy.com>
2020-02-23 23:17:56 +0100

    Updated db.go to contain common and main interfaces for db layer

--- secretdb/db/db.go
+++ secretdb/db/db.go
@@ -1,84 +1,38 @@
 package db
 
+// db.go provides interfaces for collection based
+// databases.
+
 import (
 	"context"
-	"strings"
-	"time"
 
-	"github.com/RedeployAB/burnit/secretdb/config"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/RedeployAB/burnit/secretdb/internal/models"
 )
 
-// Connection represents a connection to a database.
+// Connection provides methods Connect and Disconnect.
 type Connection interface {
 	Connect(context.Context) error
 	Disconnect(context.Context) error
 }
 
-// Client is a wrapper around mongo.Client.
-type Client struct {
-	*mongo.Client
+// Client provides methods Database, Connect,
+// and Disconnect().
+type Client interface {
+	Database(name string) Database
+	Connect(context.Context) error
+	Disconnect(context.Context) error
 }
 
-// Collection is a wrapper around mongo.Collection.
-type Collection struct {
-	*mongo.Collection
+// Database provides method Collection.
+type Database interface {
+	Collection(name string) Collection
 }
 
-// Connect is used to connect to database with options
-// specified in the passed in options argument.
-func Connect(opts config.Database) (*Client, error) {
-	uri := opts.URI
-	if len(uri) == 0 {
-		uri = toURI(opts)
-	}
-
-	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
-	if err != nil {
-		return nil, err
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	if err := client.Connect(ctx); err != nil {
-		return nil, err
-	}
-
-	if err := client.Ping(ctx, nil); err != nil {
-		return nil, err
-	}
-
-	return &Client{client}, nil
-}
-
-// Close disconnects the connection to the database.
-func Close(c Connection) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
-
-	if err := c.Disconnect(ctx); err != nil && err != mongo.ErrClientDisconnected {
-		return err
-	}
-	return nil
-}
-
-func toURI(opts config.Database) string {
-	var b strings.Builder
-
-	b.WriteString("mongodb://")
-	if opts.Username != "" {
-		b.WriteString(opts.Username)
-		if opts.Password != "" {
-			b.WriteString(":" + opts.Password)
-		}
-		b.WriteString("@")
-	}
-	b.WriteString(opts.Address)
-	if opts.SSL != false {
-		b.WriteString("/?ssl=true")
-	}
-
-	return b.String()
+// Collection provides methods FindOne, InsertOne,
+// DeleteOne and DeleteMany.
+type Collection interface {
+	FindOne(id string) (*models.Secret, error)
+	InsertOne(s *models.Secret) (*models.Secret, error)
+	DeleteOne(id string) (int64, error)
+	DeleteMany() (int64, error)
 }
