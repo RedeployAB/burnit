2bd7d1422892cddfc176fe7366ecfd7f8cfcddd0
Karl Wallenius <karl.wallenius@redeploy.com>
2020-10-11 20:28:10 +0200

    Add basic deploy script for kubernetes

---
+++ deployments/kubernetes/deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+NAMESPACE=burnit
+
+for arg in "$@"
+do
+  case $arg in
+    --burnitdb-config)
+      shift
+      BURNITDB_CONFIG=$1
+      shift
+      ;;
+    --burnitgw-config)
+      shift
+      BURNITGW_CONFIG=$1
+      shift
+      ;;
+  esac
+done
+
+if [[ -z $BURNITDB_CONFIG || -z $BURNITGW_CONFIG ]]; then
+  echo "config file path for both burnitdb and burnitgw must be provided"
+  exit 1
+fi
+
+MANIFESTS=`dirname "$0"`
+
+cat $MANIFESTS/burnitgen/deployment.yaml
+
+kubectl create namespace $BURNIT
+
+kubectl create secret generic burnitdb-config \
+  --from-file=$BURNITDB_CONFIG \
+  --namespace $NAMESPACE
+
+kubectl create secret generic burnitgw-config \
+  --from-file=$BURNITGW_CONFIG \
+  --namespace $NAMESPACE
+
+kubectl apply -f $MANIFESTS/burnitgen/deployment.yaml --namespace $NAMESPACE
+kubectl apply -f $MANIFESTS/burnitgen/service.yaml --namespace $NAMESPACE
+
+kubectl apply -f $MANIFESTS/burnitdb/deployment.yaml --namespace $NAMESPACE
+kubectl apply -f $MANIFESTS/burnitdb/service.yaml --namespace $NAMESPACE
+
+kubectl apply -f $MANIFESTS/burnitgw/deployment.yaml --namespace $NAMESPACE
+kubectl apply -f $MANIFESTS/burnitgw/service.yaml --namespace $NAMESPACE
