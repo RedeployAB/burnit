c7fc90107beadacb0b1844d358e424107db8d7bc
Karl Wallenius <karl.wallenius@redeploy.com>
2020-12-29 10:40:12 +0100

    Update build scripts, remove makefiles

--- burnitgen/build.sh
+++ burnitgen/build.sh
@@ -1,23 +1,68 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Specify a version as first argument, as 'x.x.x'."
-    exit 1
+function help {
+  echo "Usage:"
+  echo "--version         Version of binary. Mandatory"
+  echo "--docker          Build docker image with binary"
+  echo "-p, --platform    Platform for binary. Supported: linux|darwin. Default: linux"
+  echo "-h, --help        Prints help information"
+  exit 0
+}
+
+# Set defaults.
+BIN=burnitgen
+ARCH=amd64
+OS=linux
+VERSION=""
+CONTAINER=0
+
+# Handle incoming parameters.
+for arg in "$@"
+do
+  case $arg in
+    -h|--help)
+      help
+      ;;
+    --version)
+      shift
+      VERSION=$1
+      shift
+      ;;
+    -p|--platform)
+      shift
+      if [[ $1 != "linux" && $1 != "darwin" ]]; then
+        echo "$1 is not a supported platform"
+        exit 1
+      fi
+      OS=$1
+      shift
+      ;;
+    --docker)
+      CONTAINER=1
+      shift
+      ;;
+    esac
+done
+
+if [ -z $VERSION ]; then
+  help
+  exit 1
 fi
 
-VERSION=$1
-OS=linux
-BIN=burnitgen
-# Test and build with make.
-make VERSION=$VERSION release -j3 || { echo 'FAILURE: test/build failed'; exit 1;}
-# Rename and copy file
-mkdir -p release/bin
-cp release/$BIN-$VERSION-$OS-amd64 release/bin/$BIN
-# Create docker image.
-docker build -t $BIN:$VERSION --build-arg VERSION=$VERSION .
-# Clean up dangling images.
-echo ""
-echo "Deleting dangling images..."
-docker image prune -f
-echo ""
-echo "Build complete."
+# Run tests.
+go test ./...
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+bin_full_name=$BIN-$VERSION-$OS-$ARCH
+
+mkdir -p release
+GOOS=$OS GOARCH=$ARCH go build -o release/$bin_full_name -ldflags="-w -s" -trimpath cmd/$BIN/main.go
+
+if [[ $CONTAINER -eq 1 && "$OS" == "linux" ]]; then
+  mkdir -p release/bin/$OS
+  cp release/$bin_full_name release/bin/$OS/$BIN
+  docker build -t $BIN:$VERSION --build-arg VERSION=$VERSION .
+  docker image prune -f
+fi
