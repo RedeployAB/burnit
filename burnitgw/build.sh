2211a049619d4ef45c70d1b200ab24d0e2a53cf4
Karl Wallenius <karl.wallenius@redeploy.com>
2021-05-01 19:28:15 +0200

    Update Dockerfile and build.sh burnitgw

--- burnitgw/build.sh
+++ burnitgw/build.sh
@@ -55,15 +55,15 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-bin_full_name=$BIN-$VERSION-$OS-$ARCH
-bin_path=release/$OS/bin
-
-mkdir -p release
-GOOS=$OS GOARCH=$ARCH go build -o release/$bin_full_name -ldflags="-w -s" -trimpath cmd/$BIN/main.go
-
 if [[ $CONTAINER -eq 1 && "$OS" == "linux" ]]; then
-  mkdir -p $bin_path
-  cp release/$bin_full_name $bin_path/$BIN
-  docker build -t $BIN:$VERSION --build-arg VERSION=$VERSION .
+  mkdir build
+  cp -r ../common build
+  docker build -t $BIN:$VERSION --build-arg OS=$OS --build-arg ARCH=$ARCH --build-arg BIN=$BIN --build-arg VERSION=$VERSION .
   docker image prune -f
+  rm -rf build
+else
+  bin_full_name=$BIN-$VERSION-$OS-$ARCH
+  bin_path=release/$OS/bin
+  mkdir -p release
+  GOOS=$OS GOARCH=$ARCH go build -o release/$bin_full_name -ldflags="-w -s" -trimpath cmd/$BIN/main.go
 fi
