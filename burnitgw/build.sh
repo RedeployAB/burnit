590cf046241cc7e1437665f286624a26187da7a6
Karl <karl.wallenius@redeploy.com>
2022-05-22 12:37:37 +0200

    Update dependencies (#12)
    * Update to go v1.18, update Dockerfile to match, updates to build script for burnitgen, burnitdb and burnitgw
    
    * Clean up of internal request handler and services
    
    * Remove unecessary test from burnitgen, burnitdb and burnitgw server_test.go

--- burnitgw/build.sh
+++ burnitgw/build.sh
@@ -4,15 +4,14 @@ function help {
   echo "Usage:"
   echo "--version         Version of binary. Mandatory"
   echo "--docker          Build docker image with binary"
-  echo "-p, --platform    Platform for binary. Supported: linux|darwin. Default: linux"
+  echo "-p, --platform    Platform for binary (os/arch). Supported: linux|darwin/amd64|arm64. Default: linux/amd64"
   echo "-h, --help        Prints help information"
   exit 0
 }
 
 # Set defaults.
 BIN=burnitgw
-ARCH=amd64
-OS=linux
+PLATFORM=linux/amd64
 VERSION=""
 CONTAINER=0
 
@@ -30,11 +29,7 @@ do
       ;;
     -p|--platform)
       shift
-      if [[ $1 != "linux" && $1 != "darwin" ]]; then
-        echo "$1 is not a supported platform"
-        exit 1
-      fi
-      OS=$1
+      PLATFORM=$1
       shift
       ;;
     --docker)
@@ -49,21 +44,39 @@ if [ -z $VERSION ]; then
   exit 1
 fi
 
+platformParts=($(echo $PLATFORM | sed "s/\// /"))
+os=${platformParts[0]}
+arch=${platformParts[1]}
+
+if [[ $os != "linux" ]] && [[ $os != "darwin" ]]; then
+  echo "os: $os is not a supported operating system"
+  exit 1
+fi
+
+if [ -z $arch ]; then
+  arch=amd64
+fi
+
+if [[ $arch != "amd64" ]] && [[ $arch != "arm64" ]]; then
+  echo "architecture: $arch is not a supported architecture"
+  exit 1
+fi
+
 # Run tests.
 go test ./...
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
-if [[ $CONTAINER -eq 1 && "$OS" == "linux" ]]; then
+if [[ $CONTAINER -eq 1 && "$os" == "linux" ]]; then
   mkdir build
   cp -r ../common build
-  docker build -t $BIN:$VERSION --build-arg OS=$OS --build-arg ARCH=$ARCH --build-arg BIN=$BIN --build-arg VERSION=$VERSION .
+  docker build -t $BIN:$VERSION --build-arg OS=$os --build-arg ARCH=$arch --build-arg BIN=$BIN --build-arg VERSION=$VERSION --platform $os/$arch .
   docker image prune -f
   rm -rf build
 else
-  bin_full_name=$BIN-$VERSION-$OS-$ARCH
-  bin_path=release/$OS/bin
+  bin_full_name=$BIN-$VERSION-$os-$arch
+  bin_path=release/$os/bin
   mkdir -p release
-  GOOS=$OS GOARCH=$ARCH go build -o release/$bin_full_name -ldflags="-w -s" -trimpath cmd/$BIN/main.go
+  GOOS=$os GOARCH=$arch go build -o $bin_path/$bin_full_name -ldflags="-w -s" -trimpath cmd/$BIN/main.go
 fi
