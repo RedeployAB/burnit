b3f5a2a6a9aceb192c0ef9f0f98d9d9fac8e77f9
Karl Wallenius <karl.wallenius@redeploy.com>
2022-12-10 16:39:14 +0100

    Update name Merge to merge, fix bug in setting booleans in sub structs

--- burnit/config/merge.go
+++ burnit/config/merge.go
@@ -5,9 +5,9 @@ import (
 	"reflect"
 )
 
-// Merge two structs. Values in src will overwrite those in dst, provided
+// merge two structs. Values in src will overwrite those in dst, provided
 // that they do not contains the zero value for it's type.
-func Merge(dst any, src any) error {
+func merge(dst any, src any) error {
 	dstv := reflect.ValueOf(dst)
 	if dstv.Kind() != reflect.Pointer || dstv.Elem().Kind() != reflect.Struct {
 		return errors.New("dst must be a pointer to a struct")
@@ -31,16 +31,13 @@ func Merge(dst any, src any) error {
 // setFields sets values from src into dst provided that they are not their
 // zero value for it's type.
 func setFields(dst reflect.Value, src reflect.Value) {
-	if isEmpty(src) {
-		return
-	}
 	if src.Kind() == reflect.Pointer {
+		if src.IsNil() {
+			return
+		}
 		src = src.Elem()
 	}
 	for i := 0; i < src.NumField(); i++ {
-		if isEmpty(src.Field(i)) && src.Field(i).Kind() != reflect.Bool {
-			continue
-		}
 		if src.Field(i).Kind() == reflect.Pointer && src.Field(i).Elem().Kind() == reflect.Struct {
 			setFields(dst.Field(i).Elem(), src.Field(i).Elem())
 		} else if src.Field(i).Kind() == reflect.Struct {
@@ -57,20 +54,9 @@ func setValue(dst reflect.Value, src reflect.Value) {
 		if dst.IsNil() {
 			dst.Set(reflect.New(dst.Type()).Elem())
 		}
-	} else if dst.Kind() == reflect.Struct {
-		if dst.IsZero() {
-			dst.Set(reflect.New(dst.Type().Elem()))
-		}
+	}
+	if src.IsZero() && src.Kind() != reflect.Bool {
+		return
 	}
 	dst.Set(src)
-}
-
-// isEmpty checks if the provided reflect.Value is either
-// nil if a pointer, or zero value for all other cases.
-func isEmpty(v reflect.Value) bool {
-	if v.Kind() == reflect.Pointer {
-		return v.IsNil()
-	} else {
-		return v.IsZero()
-	}
 }
