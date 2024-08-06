b8ba221029495dfd34c3d2fb5d2a5ab7d9fe142d
Karl Wallenius <karl.wallenius@redeploy.com>
2024-08-06 08:35:43 +0200

    feat: add passphrase to create request for secrets

--- api/secret.go
+++ api/secret.go
@@ -1,22 +1,27 @@
 package api
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 // Secret represents a secret.
 type Secret struct {
-	ID    string        `json:"id,omitempty"`
-	Value string        `json:"value,omitempty"`
-	TTL   time.Duration `json:"ttl,omitempty"`
+	ID        string        `json:"id,omitempty"`
+	Value     string        `json:"value,omitempty"`
+	TTL       time.Duration `json:"ttl,omitempty"`
+	ExpiresAt *time.Time    `json:"expiresAt,omitempty"`
 }
 
 // CreateSecretRequest represents a request to create a secret.
 type CreateSecretRequest struct {
-	Value string        `json:"value,omitempty"`
-	TTL   time.Duration `json:"ttl,omitempty"`
+	Value      string        `json:"value,omitempty"`
+	Passphrase string        `json:"passphrase,omitempty"`
+	TTL        time.Duration `json:"ttl,omitempty"`
 }
 
 // Valid validates the CreateSecretRequest.
-func (r CreateSecretRequest) Valid() map[string]string {
+func (r CreateSecretRequest) Valid(ctx context.Context) map[string]string {
 	errs := make(map[string]string)
 	if len(r.Value) == 0 {
 		errs["value"] = "value is required"
