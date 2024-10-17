5dc46c3d2e1b45d15b183369c02d6bdb2e59db87
Karl Wallenius <karl.wallenius@redeploy.com>
2024-10-18 00:44:37 +0200

    feat: redirect to root on handlers (htmx) when not an HX request

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -27,11 +27,27 @@ func (s *server) routes() {
 	s.router.Handle("POST /secrets", s.createSecret())
 	s.router.Handle("DELETE /secrets/", s.deleteSecret())
 
+	if s.backendOnly {
+		return
+	}
+
 	// Frontend routes and handlers.
-	s.router.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	s.router.Handle("GET /", s.uiCreateSecret())
-	s.router.Handle("GET /ui/secrets/", s.uiGetSecret())
+	s.router.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	s.router.Handle("/", s.uiCreateSecret())
+	s.router.Handle("/ui/secrets/", s.uiGetSecret())
 
 	// HTMX routes and handlers.
-	s.router.Handle("POST /handlers/secret/create", s.handlerCreateSecret())
+	s.router.Handle("/handlers/secret/create", htmxHandler(s.handlerCreateSecret()))
+}
+
+// htmxHandler is a middleware that ensures the request is an htmx request.
+// If it is not, the request is redirected to the root.
+func htmxHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Hx-Request") != "true" {
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
