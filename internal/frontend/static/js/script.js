6d923bdde9bee00d1038aabdd12a3b1252e2a3c6
Karl <karl.wallenius@redeploy.com>
2024-11-01 01:09:55 +0100

    feat: base and create secret UI page design (#57)
    * feat: improve base page and creation form
    
    * feat: secret result links with overlay

--- internal/frontend/static/js/script.js
+++ internal/frontend/static/js/script.js
@@ -26,7 +26,22 @@ function copyToClipboard(elementId, feedbackElementId) {
   navigator.clipboard.writeText(text).then(() => {
     if (feedbackElementId) {
       const feedback = document.getElementById(feedbackElementId);
-      feedback.innerText = "Copied to clipboard!";
+      feedback.disabled = true;
+
+      feedback.classList.remove('hover:text-gray-200');
+      const innerHTML = feedback.innerHTML;
+
+      feedback.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-green-600">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+        </svg>
+      `;
+
+      setTimeout(() => {
+        feedback.innerHTML = innerHTML;
+        feedback.disabled = false;
+        feedback.classList.add('hover:text-gray-200');
+      }, 3000);
     }
   });
 }
