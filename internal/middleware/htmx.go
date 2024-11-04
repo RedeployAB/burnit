eca896341a196d58c03129ce0b1735329e8eab97
Karl <karl.wallenius@redeploy.com>
2024-11-05 00:14:26 +0100

    feat: move middleware package (#64)
    * feat: move middleware package to outside server
    
    * feat: move htmx handler middleware to middleware package

--- internal/middleware/htmx.go
+++ internal/middleware/htmx.go
@@ -1,10 +1,10 @@
-package frontend
+package middleware
 
 import "net/http"
 
-// HTMXHandler is a middleware that ensures the request is an htmx request.
+// HTMX is a middleware that ensures the request is an htmx request.
 // If it is not, the request is redirected to the root.
-func HTMXHandler(next http.Handler) http.Handler {
+func HTMX(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("Hx-Request") != "true" {
 			http.Redirect(w, r, "/", http.StatusFound)
