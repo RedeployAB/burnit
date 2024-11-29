2498324153e25f62d416c8d6b80b6843988477a0
Karl <karl.wallenius@redeploy.com>
2024-11-29 23:43:45 +0100

    feat: placing and colors (#88)
    * feat: icon sizes and font sizes
    
    * feat: size of inputs and labels

--- internal/ui/static/js/script.js
+++ internal/ui/static/js/script.js
@@ -210,7 +210,7 @@ function copyToClipboard(elementId, feedbackElementId) {
       const innerHTML = feedback.innerHTML;
 
       feedback.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-green-600">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-green-600">
           <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
         </svg>
       `;
