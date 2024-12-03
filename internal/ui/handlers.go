51123cf455f92cad6d812a96b2d4ed6bbb4186b2
Karl Wallenius <karl.wallenius@redeploy.com>
2024-12-03 22:39:04 +0100

    chore: cleanup function locations

--- internal/ui/handlers.go
+++ internal/ui/handlers.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"regexp"
 	"strings"
 	"time"
 
@@ -260,64 +259,6 @@ func extractIDAndPassphrase(route, path string) (string, string, error) {
 		return parts[0], "", nil
 	}
 	return parts[0], parts[1], nil
-}
-
-// secretCreateResponse is the response data for a create secret request.
-type secretCreateResponse struct {
-	BaseURL        string
-	ID             string
-	Passphrase     string
-	PassphraseHash string
-	CSRFToken      string
-}
-
-// secretGetResponse is the response data for a get secret request.
-type secretGetResponse struct {
-	ID             string
-	PassphraseHash string
-	Value          string
-	CSRFToken      string
-}
-
-// errorResponse is the response data for an error.
-type errorResponse struct {
-	RequestID string
-	Title     string
-	Message   string
-}
-
-// formatErrorMessage formats an error message.
-func formatErrorMessage(err error) string {
-	if err == nil {
-		return ""
-	}
-	msg := err.Error()
-	re := regexp.MustCompile(`^.*:\s+`)
-	msg = re.ReplaceAllString(msg, "")
-	if !strings.HasSuffix(msg, ".") {
-		msg += "."
-	}
-	return strings.ToUpper(msg[:1]) + msg[1:]
-}
-
-// isSecretBadRequestError returns true if the error is a bad request error.
-func isSecretBadRequestError(err error) bool {
-	errs := []error{
-		secret.ErrValueInvalid,
-		secret.ErrInvalidPassphrase,
-		secret.ErrValueTooManyCharacters,
-		secret.ErrInvalidExpirationTime,
-		secret.ErrPassphraseInvalid,
-		secret.ErrPassphraseTooFewCharacters,
-		secret.ErrPassphraseTooManyCharacters,
-	}
-
-	for _, e := range errs {
-		if errors.Is(err, e) {
-			return true
-		}
-	}
-	return false
 }
 
 // validateCSRFTToken validates the CSRF token in the session.
