ddf1671548725510ece8a41eac47aebddf54de57
Karl Wallenius <karl.wallenius@redeploy.com>
2024-12-25 13:57:21 +0100

    fix: tests

--- internal/db/sql/session_store_test.go
+++ internal/db/sql/session_store_test.go
@@ -28,7 +28,7 @@ func TestCreateSessionQueries(t *testing.T) {
 			},
 			want: sessionQueries{
 				selectByID:        "SELECT id, expires_at, csrf_token, csrf_expires_at FROM sessions WHERE id = $1",
-				selectByCSRFToken: "SELECT id, expires_at, csrf_token, csrf_expires_at FROM sessions WHERE csrf_token = $3",
+				selectByCSRFToken: "SELECT id, expires_at, csrf_token, csrf_expires_at FROM sessions WHERE csrf_token = $1",
 				upsert:            "INSERT INTO sessions (id, expires_at, csrf_token, csrf_expires_at) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO UPDATE SET expires_at = EXCLUDED.expires_at, csrf_token = EXCLUDED.csrf_token, csrf_expires_at = EXCLUDED.csrf_expires_at",
 				delete:            "DELETE FROM sessions WHERE id = $1",
 				deleteExpired:     "DELETE FROM sessions WHERE expires_at < NOW() AT TIME ZONE 'UTC'",
@@ -45,7 +45,7 @@ func TestCreateSessionQueries(t *testing.T) {
 			},
 			want: sessionQueries{
 				selectByID:        "SELECT ID, ExpiresAt, CSRFToken, CSRFExpiresAt FROM Sessions WHERE ID = @p1",
-				selectByCSRFToken: "SELECT ID, ExpiresAt, CSRFToken, CSRFExpiresAt FROM Sessions WHERE CSRFToken = @p3",
+				selectByCSRFToken: "SELECT ID, ExpiresAt, CSRFToken, CSRFExpiresAt FROM Sessions WHERE CSRFToken = @p1",
 				upsert:            "MERGE INTO Sessions AS target USING (VALUES (@p1, @p2, @p3, @p4)) AS source (ID, ExpiresAt, CSRFToken, CSRFExpiresAt) ON target.ID = source.ID WHEN MATCHED THEN UPDATE SET target.ExpiresAt = source.ExpiresAt, target.CSRFToken = source.CSRFToken, target.CSRFExpiresAt = source.CSRFExpiresAt WHEN NOT MATCHED THEN INSERT (ID, ExpiresAt, CSRFToken, CSRFExpiresAt) VALUES (source.ID, source.ExpiresAt, source.CSRFToken, source.CSRFExpiresAt);",
 				delete:            "DELETE FROM Sessions WHERE ID = @p1",
 				deleteExpired:     "DELETE FROM Sessions WHERE ExpiresAt < GETUTCDATE()",
@@ -62,7 +62,7 @@ func TestCreateSessionQueries(t *testing.T) {
 			},
 			want: sessionQueries{
 				selectByID:        "SELECT id, expires_at, csrf_token, csrf_expires_at FROM sessions WHERE id = ?1",
-				selectByCSRFToken: "SELECT id, expires_at, csrf_token, csrf_expires_at FROM sessions WHERE csrf_token = ?3",
+				selectByCSRFToken: "SELECT id, expires_at, csrf_token, csrf_expires_at FROM sessions WHERE csrf_token = ?1",
 				upsert:            "INSERT INTO sessions (id, expires_at, csrf_token, csrf_expires_at) VALUES (?1, ?2, ?3, ?4) ON CONFLICT(id) DO UPDATE SET expires_at = excluded.expires_at, csrf_token = excluded.csrf_token, csrf_expires_at = excluded.csrf_expires_at",
 				delete:            "DELETE FROM sessions WHERE id = ?1",
 				deleteExpired:     "DELETE FROM sessions WHERE expires_at < DATETIME('now')",
