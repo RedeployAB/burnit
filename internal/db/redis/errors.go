b3eb4e0cf700deae464a8ae74a6504fe4e80df9e
Karl Wallenius <karl.wallenius@redeploy.com>
2025-01-21 20:51:38 +0100

    feat: remove unecessary error types from redis store

--- internal/db/redis/errors.go
+++ internal/db/redis/errors.go
@@ -3,8 +3,6 @@ package redis
 import "errors"
 
 var (
-	// ErrNilClient is returned when the client is nil.
-	ErrNilClient = errors.New("client is nil")
 	// ErrKeyNotFound is returned when the key is not found.
 	ErrKeyNotFound = errors.New("key not found")
 )
