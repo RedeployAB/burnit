a16f4e1d0e6de5eec93b36a04a139d5d443225fd
Karl <5736625+KarlGW@users.noreply.github.com>
2025-01-29 21:05:47 +0100

    cicd: build and release workflow (#126)
    * cicd: update build script to include archiving
    
    * cicd: build and release workflow

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 expected_esbuild_sha256=$ESBUILD_SHA256
-expected_htmx_sha256=$HTMX_SHA256
 expected_tailwindcss_sha256=$TAILWINDCSS_SHA256
-curr_dir=$(pwd)
+expected_htmx_sha256=$HTMX_SHA256
+
+cwd=$(pwd)
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed_flags=(-i '')
@@ -11,6 +12,7 @@ else
 fi
 
 # Download esbuild if it doesn't exist and verify the SHA256 checksum.
+esbuild="esbuild"
 if [ -z "$(command -v esbuild)" ]; then
     curl -fsSL https://esbuild.github.io/dl/v0.24.0 | sh
     if [ $? -ne 0 ]; then
@@ -18,14 +20,17 @@ if [ -z "$(command -v esbuild)" ]; then
         exit 1
     fi
 
-    $actual_esbuild_sha256=$(sha256sum esbuild | awk '{print $1}')
-    if [ "$expected_esbuild_sha256" != "actual_esbuild_sha256" ]; then
+    actual_esbuild_sha256=$(sha256sum ./esbuild | awk '{print $1}')
+    if [ "$expected_esbuild_sha256" != "$actual_esbuild_sha256" ]; then
         echo "SHA256 checksum mismatch for esbuild"
         exit 1
     fi
+    chmod +x ./esbuild
+    esbuild=./$esbuild
 fi
 
 # Download tailwindcss if it doesn't exist and verify the SHA256 checksum.
+tailwindcss="tailwindcss"
 if [ -z "$(command -v tailwindcss)" ]; then
   curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/download/v3.4.14/tailwindcss-linux-x64
   if [ $? -ne 0 ]; then
@@ -33,14 +38,15 @@ if [ -z "$(command -v tailwindcss)" ]; then
     exit 1
   fi
   
-  chmod +x tailwindcss-linux-x64
-  mv tailwindcss-linux-x64 tailwindcss
+  chmod +x ./tailwindcss-linux-x64
+  mv ./tailwindcss-linux-x64 ./tailwindcss
 
-  $actual_tailwindcss_sha256=$(sha256sum tailwindcss | awk '{print $1}')
+  actual_tailwindcss_sha256=$(sha256sum ./tailwindcss | awk '{print $1}')
   if [ "$expected_tailwindcss_sha256" != "$actual_tailwindcss_sha256" ]; then
     echo "SHA256 checksum mismatch for tailwindcss"
     exit 1
   fi
+  tailwindcss=../../$tailwindcss
 fi
 
 
@@ -57,12 +63,12 @@ fi
 
 # Build CSS with tailwindcss.
 cd internal/ui
-tailwindcss -i ./static/css/tailwind.css -o ./static/css/main.css
-cd $curr_dir
+$tailwindcss -i ./static/css/tailwind.css -o ./static/css/main.css
+cd $cwd
 
 # Bundle JS and CSS.
-esbuild internal/ui/static/js/main.js --bundle --minify --outfile=internal/ui/static/js/main.min.js
-esbuild internal/ui/static/css/main.css --bundle --minify --outfile=internal/ui/static/css/main.min.css
+$esbuild internal/ui/static/js/main.js --bundle --minify --outfile=internal/ui/static/js/main.min.js
+$esbuild internal/ui/static/css/main.css --bundle --minify --outfile=internal/ui/static/css/main.min.css
 
 gzip -k -f internal/ui/static/js/main.min.js
 gzip -k -f internal/ui/static/css/main.min.css
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 bin=burnit
 module_path=github.com/RedeployAB/$bin
-os=linux
-arch=amd64
-build_root=build/
-bin_path=$build_root/$bin
+build_root=build
+platform=linux/amd64
+
 version=""
-container=0
+image=0
+archive=0
+cleanup=1
+skip_tests=0
 
 for arg in "$@"
 do
@@ -16,22 +18,31 @@ do
       version=$1
       shift
       ;;
-    --os)
+    --platform)
       shift
-      os=$1
-      shift
-      ;;
-    --arch)
-      shift
-      arch=$1
+      platform=$1
       shift
       ;;
     --image)
-      container=1
+      image=1
+      shift
+      ;;
+    --archive)
+      archive=1
+      shift
+      ;;
+    --no-cleanup)
+      cleanup=0
+      shift
+      ;;
+    --skip-tests)
+      skip_tests=1
       shift
       ;;
   esac
 done
+
+cwd=$(pwd)
 
 if [ -z $bin ]; then
   bin=$(echo $(basename $(pwd)))
@@ -46,22 +57,42 @@ if [ -z $build_root ]; then
   exit 1
 fi
 
-if [ -d $build_root ]; then
+if [ $cleanup -eq 1 ] && [ -d $build_root ]; then
   rm -rf $build_root/*
 fi
 
 mkdir -p $build_root
 
-go test ./...
-if [ $? -ne 0 ]; then
-  exit 1
+if [ $skip_tests -eq 0 ]; then
+  go test ./...
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
 fi
 
-CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build \
-  -o $bin_path \
+platforms=(${platform//,/ })
+for p in "${platforms[@]}"; do
+  os_arch=(${p//\// })
+  os=${os_arch[0]}
+  arch=${os_arch[1]}
+
+  bin_path=$build_root/$os/$arch
+  CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build \
+  -o $bin_path/$bin \
   -ldflags="-s -w -X '$module_path/internal/version.version=$version'" \
   -trimpath main.go
 
-if [ $container -eq 1 ] && [ "$os" == "linux" ]; then
-  docker build -t $bin:$version --platform $os/$arch .
+  if [ $archive -eq 1 ]; then
+    cd $bin_path
+    targz=$bin-$version-$os-$arch.tar.gz
+    tar -czf $targz *
+    mv $targz $cwd/$build_root/$targz
+    cd $cwd
+  fi
+done
+
+if [ $image -eq 1 ]; then
+  docker buildx create --name multiarch --use --bootstrap
+  docker build -t $bin:$version --platform $platform .
+  docker buildx rm multiarch
 fi
